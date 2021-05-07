7d54bb70444d46a1e723c7fa461e8fdb2c77f791
陈子豪 <1417376552@qq.com>
2021-05-07 20:04:07 +0800

    数据结构的学习

--- 数据结构/单链表/src/MyLinkedList.java
+++ 数据结构/单链表/src/MyLinkedList.java
@@ -34,28 +34,56 @@ public class MyLinkedList {
     }
 
     //查找关键字key是否在单链表中
-    public boolean contains(int key){
-        Node cur=this.head;
-        while(cur!=null){
-            if(cur.value==key){
+    public boolean contains(int key) {
+        Node cur = this.head;
+        while (cur != null) {
+            if (cur.value == key) {
                 return true;
             }
-            cur=cur.next;
+            cur = cur.next;
         }
         return false;
     }
 
     //删除第一次出现关键字key的节点
-    public void remove(int key){
-        Node cur=this.head;
-        while(cur!=null){
-            if(cur.next.value==key){
-                break;
-            }
-            cur=cur.next;
+    public void remove(int key) {
+        Node cur = this.head;
+        if (cur == null) {
+            return;
         }
-        assert cur != null;
-        cur.next=cur.next.next;
+        //删除头结点的key关键字
+        if (cur.value == key) {
+            this.head = cur.next;
+            return;
+        }
+        //从头结点下一个结点开始遍历
+        while (cur.next != null) {
+            if (cur.next.value == key) {
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
+    }
+
+    //删除所有出现关键字key的节点
+    public void removeAllKey(int key) {
+        Node cur = this.head;
+        if (cur == null) {
+            return;
+        }
+        //从头结点下一个结点开始遍历
+        while (cur.next != null) {
+            if (key == cur.next.value) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        //删除头结点的key关键字
+        if (this.head.value == key) {
+            this.head = this.head.next;
+        }
     }
 
 
@@ -70,32 +98,32 @@ public class MyLinkedList {
     }
 
     //任意位置插入（以第一个数据节点为0号下标为准）
-    public void addIndex(int index,int data){
-        if(index==0){
+    public void addIndex(int index, int data) {
+        if (index == 0) {
             this.addFirst(data);
             return;
         }
-        if(index==this.size()){
+        if (index == this.size()) {
             this.addLast(data);
             return;
         }
 
-        Node node=new Node(data);
-        Node cur=this.head;
-        int count=0;
+        Node node = new Node(data);
+        Node cur = this.head;
+        int count = 0;
         //找到index位置的前一个节点
         while (count + 1 != index) {
             cur = cur.next;
             count++;
         }
-        node.next=cur.next;
-        cur.next=node;
+        node.next = cur.next;
+        cur.next = node;
     }
 
 
     //清空单链表
-    public void clear(){
-        this.head=null;
+    public void clear() {
+        this.head = null;
     }
 
     //计算链表长度

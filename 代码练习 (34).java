953cc0a357cbf1da973f783e7131865e933a2da2
陈子豪 <1417376552@qq.com>
2021-06-01 22:28:35 +0800

    日常练习

---
+++ 代码练习 (34).java
@@ -0,0 +1,28 @@
+// 判断给定的链表中是否有环。如果有环则返回true，否则返回false。
+//你能给出空间复杂度的解法么？ 
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

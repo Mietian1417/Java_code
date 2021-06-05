93b5ac214b01defab455495b2f0a1f04248c949c
陈子豪 <1417376552@qq.com>
2021-06-05 19:24:09 +0800

    日常练习

---
+++ 代码练习 (37).java
@@ -0,0 +1,34 @@
+//给你一个链表的头节点 head 和一个整数 val ，
+//请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+
+ 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return null;
+        }
+        ListNode cur = head;
+        while(cur.next  != null){
+            if(cur.next.val == val){
+                cur.next = cur.next.next;
+            }else{
+                 cur = cur.next;
+            }
+        }
+        if(head.val == val){
+            head = head.next;
+        }
+        return head;
+    }
+}

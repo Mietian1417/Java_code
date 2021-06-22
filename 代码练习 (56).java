88e41756410353c30e8595128630c313fc02978a
陈子豪 <1417376552@qq.com>
2021-06-22 14:53:18 +0800

    日常练习

---
+++ 代码练习 (56).java
@@ -0,0 +1,79 @@
+package 日常练习.寻找两个正序数组的中位数;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 陈子豪
+ * Date: 2021-06-22
+ * Time: 14:16
+ */
+
+//给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+//
+//
+//
+//示例 1：
+//
+//输入：nums1 = [1,3], nums2 = [2]
+//输出：2.00000
+//解释：合并数组 = [1,2,3] ，中位数 2
+//
+//示例 2：
+//
+//输入：nums1 = [1,2], nums2 = [3,4]
+//输出：2.50000
+//解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+//
+//示例 3：
+//
+//输入：nums1 = [0,0], nums2 = [0,0]
+//输出：0.00000
+//
+//示例 4：
+//
+//输入：nums1 = [], nums2 = [1]
+//输出：1.00000
+//
+//示例 5：
+//
+//输入：nums1 = [2], nums2 = []
+//输出：2.00000
+//
+//
+//
+//提示：
+//
+//    nums1.length == m
+//    nums2.length == n
+//    0 <= m <= 1000
+//    0 <= n <= 1000
+//    1 <= m + n <= 2000
+//    -106 <= nums1[i], nums2[i] <= 106
+//
+//
+//
+//进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] array = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, array, 0, nums1.length);
+
+        System.arraycopy(nums2, 0, array, nums1.length, nums2.length);
+
+        Arrays.sort(array);
+        int length = array.length;
+        if (array.length % 2 == 0) {
+            return (array[length / 2] + array[length / 2 - 1]) / 2.0;
+        }
+        return array[length / 2];
+    }
+}

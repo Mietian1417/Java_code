9f3cf13bf210270a52fe16314b3c33bcef9d4479
陈子豪 <1417376552@qq.com>
2021-07-21 10:14:00 +0800

    日常练习

---
+++ 代码练习 (59).java
@@ -0,0 +1,96 @@
+package 日常练习.二叉树的前序遍历;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 陈子豪
+ * Date: 2021-06-20
+ * Time: 15:35
+ */
+
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+//提示：
+//
+//    树中节点数目在范围 [0, 100] 内
+//    -100 <= Node.val <= 100
+//
+//
+//
+//进阶：递归算法很简单，你可以通过迭代算法完成吗？
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+方法一：
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        preorder(root, res);
+        return res;
+    }
+
+    public void preorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preorder(root.left, res);
+        preorder(root.right, res);
+    }
+}*/
+
+
+/*
+方法二：
+class Solution {
+    List<Integer> res = new ArrayList<Integer>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        res.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return res;
+    }
+}*/
+
+/*
+方法三：
+class Solution {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        res.add(root.val);
+
+        List<Integer> leftTree = preorderTraversal(root.left);
+        res.addAll(leftTree);
+
+        List<Integer> rightTree = preorderTraversal(root.right);
+        res.addAll(rightTree);
+
+        return res;
+    }
+
+}*/
---
+++ 代码练习 (61).java
@@ -0,0 +1,89 @@
+package 日常练习.二叉树的后序遍历;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 陈子豪
+ * Date: 2021-06-20
+ * Time: 15:53
+ */
+
+//给定一个二叉树，返回它的 后序 遍历。
+//
+//进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/binary-tree-postorder-traversal
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+方法一：
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        after(root,list);
+        return list;
+    }
+
+    public void after(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+        after(root.left,list);
+        after(root.right,list);
+        list.add(root.val);
+    }
+}*/
+
+
+/*
+方法二：
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer>  postorderTraversal(TreeNode root) {
+        if(root == null){
+            return res;
+        }
+         postorderTraversal(root.left);
+         postorderTraversal(root.right);
+        res.add(root.val);
+        return res;
+    }
+}*/
+
+/*
+方法三：
+class Solution {
+    public List<Integer>  postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        List<Integer> leftTree = postorderTraversal(root.left);
+        res.addAll(leftTree);
+
+        List<Integer> rightTree = postorderTraversal(root.right);
+        res.addAll(rightTree);
+
+        res.add(root.val);
+
+        return res;
+    }
+}*/
